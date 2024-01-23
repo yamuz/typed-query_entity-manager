944c4ac7197429547300f9ef66d254e33a6ee9b0
Almaz Abdrayev <yamuuzz@gmail.com>
2024-01-23 16:30:39 +0600

    feat -all files

---
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -0,0 +1,118 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Author;
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.Query;
+import java.util.List;
+
+
+/**
+ * Created by jt on 8/28/21.
+ */
+@Component
+public class AuthorDaoImpl implements AuthorDao {
+
+    private final EntityManagerFactory emf;
+
+    public AuthorDaoImpl(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    @Override
+    public List<Author> listAuthorByLastNameLike(String lastName) {
+        EntityManager em = getEntityManager();
+
+        try {
+            Query query = em.createQuery("SELECT a from Author a where a.lastName like :last_name");
+            query.setParameter("last_name", lastName + "%");
+            List<Author> authors = query.getResultList();
+
+            return authors;
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public Author getById(Long id) {
+        EntityManager em = getEntityManager();
+        Author author = getEntityManager().find(Author.class, id);
+        em.close();
+        return author;
+    }
+
+    @Override
+    public Author findAuthorByName(String firstName, String lastName) {
+        EntityManager em = getEntityManager();
+        TypedQuery<Author> query = em.createQuery("SELECT a FROM Author a " +
+                "WHERE a.firstName = :first_name and a.lastName = :last_name", Author.class);
+        query.setParameter("first_name", firstName);
+        query.setParameter("last_name", lastName);
+
+        Author author = query.getSingleResult();
+        em.close();
+        return author;
+    }
+
+    @Override
+    public Author saveNewAuthor(Author author) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        em.persist(author);
+        em.flush();
+        em.getTransaction().commit();
+        em.close();
+        return author;
+    }
+
+    @Override
+    public Author updateAuthor(Author author) {
+        EntityManager em = getEntityManager();
+
+        try {
+            em.joinTransaction();
+            em.merge(author);
+            em.flush();
+            em.clear();
+            Author saveAuthor = em.find(Author.class, author.getId());
+            return saveAuthor;
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public void deleteAuthorById(Long id) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        Author author = em.find(Author.class, id);
+        em.remove(author);
+        em.flush();
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    private EntityManager getEntityManager(){
+        return emf.createEntityManager();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
